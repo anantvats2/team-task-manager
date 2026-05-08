1713d150e545b0cdad87174cb77b33af946345ed
Anant Vats <vatsanantsanyam@gmail.com>
2026-05-08 23:03:03 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -41,8 +41,8 @@ app.use((req, res) => {
 app.use(errorHandler)
 //railway deployment fix
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
